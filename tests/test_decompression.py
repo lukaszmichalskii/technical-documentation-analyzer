ac4316ea362b9435821dd71790698c7a5b7ce501
Lukasz Michalski <lukasz.michalski222@gmail.com>
2023-03-12 23:50:57 +0100

    SAM-15: remove mypy check, fix tests failing due to different file system

--- tests/test_decompression.py
+++ tests/test_decompression.py
@@ -12,26 +12,26 @@ class TestDecompression(unittest.TestCase):
         self.archives = pathlib.Path(
             os.path.dirname(os.path.abspath(__file__))
         ).joinpath("resources/archives")
-        self.expected_dir = ["lorem-ipsum.pdf", "text1.txt", "sample.pdf", "text2.txt"]
-        self.expected_archive = ["lorem-ipsum.pdf", "text1.txt", "text2.txt"]
+        self.expected_dir = sorted(["lorem-ipsum.pdf", "text1.txt", "sample.pdf", "text2.txt"])
+        self.expected_archive = sorted(["lorem-ipsum.pdf", "text1.txt", "text2.txt"])
 
     def test_extract_tar(self) -> None:
         tarfile_ = self.archives.joinpath("tarfile.tar.xz")
         with tempfile.TemporaryDirectory(dir=self.archives) as temp:
             decompress(tarfile_, pathlib.Path(temp))
             actual = files_in_dir(pathlib.Path(temp))
-            self.assertEqual(self.expected_archive, actual)
+            self.assertEqual(self.expected_archive, sorted(actual))
 
     def test_extract_zip(self) -> None:
         zipfile_ = self.archives.joinpath("zipfile.zip")
         with tempfile.TemporaryDirectory(dir=self.archives) as temp:
             decompress(zipfile_, pathlib.Path(temp))
             actual = files_in_dir(pathlib.Path(temp))
-            self.assertEqual(self.expected_archive, actual)
+            self.assertEqual(self.expected_archive, sorted(actual))
 
     def test_copyfileobj(self) -> None:
         files_ = self.archives.parent.joinpath("dir")
         with tempfile.TemporaryDirectory(dir=self.archives) as temp:
             copyfileobj(files_, pathlib.Path(temp))
             actual = files_in_dir(pathlib.Path(temp))
-            self.assertEqual(self.expected_dir, actual)
+            self.assertEqual(self.expected_dir, sorted(actual))
