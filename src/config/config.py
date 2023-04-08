85b8d9178bf825a7e1acd8375fdbf556ff152374
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2023-04-08 13:06:58 +0200

    SAM-17: add Config class

---
+++ src/config/config.py
@@ -0,0 +1,8 @@
+import os
+
+
+class Config:
+    STARDOG_ENDPOINT = os.environ.get('STARDOG_ENDPOINT')
+    STARDOG_USERNAME = os.environ.get('STARDOG_USERNAME')
+    STARDOG_PASSWORD = os.environ.get('STARDOG_PASSWORD')
+    STARDOG_DATABASE = os.environ.get('STARDOG_DATABASE')
