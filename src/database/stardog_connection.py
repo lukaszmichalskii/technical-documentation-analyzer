f5ba0a553039771a7860f46daca72cfd79d6ab17
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2023-04-11 22:19:37 +0200

    SAM-17: fix stardog_connection.py

--- src/database/stardog_connection.py
+++ src/database/stardog_connection.py
@@ -7,23 +7,25 @@ class StardogConnection:
     Class for initializing connection with Stardog database based on environment parameters
     """
 
-    def __init__(self, config):
+    def __init__(self, config, db_name=None, is_admin=False):
         self.endpoint = config.STARDOG_ENDPOINT
         self.username = config.STARDOG_USERNAME
         self.password = config.STARDOG_PASSWORD
         self.logger = logs.setup_logger()
+        self.db_name = db_name
+        self.is_admin = is_admin
         self.connection = None
 
-    def __enter__(self, db_name=None, is_admin=False):
-        self.logger.info(f"Connecting to database {db_name}")
-        if is_admin:
+    def __enter__(self):
+        self.logger.info(f"Connecting to database {self.db_name}")
+        if self.is_admin:
             self.connection = stardog.Admin(self.endpoint, self.username, self.password)
             self.logger.info(f"Successfully established admin connection")
         else:
-            if not db_name:
+            if not self.db_name:
                 raise ValueError("db_name must be provided for non-admin connection")
-            self.connection = stardog.Connection(db_name, self.endpoint, self.username, self.password)
-            self.logger.info(f"Successfully connected to database {db_name}")
+            self.connection = stardog.Connection(self.db_name, self.endpoint, self.username, self.password)
+            self.logger.info(f"Successfully connected to database {self.db_name}")
         return self.connection
 
     def __exit__(self, exc_type, exc_val, exc_tb):
