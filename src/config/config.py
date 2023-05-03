2aca7505cd1a8636ef505e553d1cc8b2c728c666
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2023-05-03 16:46:05 +0200

    SAM-17: remove database name from config

--- src/config/config.py
+++ src/config/config.py
@@ -5,4 +5,3 @@ class Config:
     STARDOG_ENDPOINT = os.environ.get("STARDOG_ENDPOINT")
     STARDOG_USERNAME = os.environ.get("STARDOG_USERNAME")
     STARDOG_PASSWORD = os.environ.get("STARDOG_PASSWORD")
-    STARDOG_DATABASE = os.environ.get("STARDOG_DATABASE")
