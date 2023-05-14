96d25a5b129e63efd5067e30c995a1c9ffbbc34a
Lukasz Michalski <lukasz.michalski222@gmail.com>
2023-05-14 16:30:22 +0200

    SAM-43: utilize TFIDF

---
+++ src/nlp/pre_processing.py
@@ -0,0 +1,9 @@
+import re
+
+
+def remove_special_characters(text: str) -> str:
+    return re.sub(r"[\n\r\t]", " ", text)
+
+
+def remove_unicode(text: str) -> str:
+    return re.sub(r"(@\[A-Za-z0-9]+)|(\s+)|([^0-9A-Za-z\.\,\-\_ \t])|(\w+:\/\/\S+)|^rt|http+?", " ", text)
